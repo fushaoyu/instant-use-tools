b57d0cd665c089d75547d31eba8274d1645b79bb
fushaoyu <fsy_950625@163.com>
2026-08-19 09:12:29 +0800

    新增加载器和提示图标，实现视频水印功能，并创建初始HTML结构
    - 新增loader-circle和triangle-alert SVG图标。
    - 实现视频水印功能，支持用户自定义水印文字。
    - 创建favicon及图标SVG文件，便于资源管理。
    - 建立应用程序的基本HTML结构，包含必要的meta标签以及样式表和脚本链接。

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createMemoryHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 const routes = [
   {
@@ -38,7 +38,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes,
 });
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,36 @@
 import { fileURLToPath } from "node:url";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_PUBLIC_PATH || "/",
-  server: {
-    open: true,
-    port: 2468,
-  },
-  plugins: [vue(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+export default defineConfig(({ mode }) => {
+  // 读取 .env / .env.production 环境变量
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    // 优先读取 VITE_PUBLIC_PATH 环境变量，没有则生产环境默认 /instant-use-tools/，开发环境 /
+    base:
+      env.VITE_PUBLIC_PATH ||
+      (mode === "production" ? "/instant-use-tools/" : "/"),
+
+    server: {
+      open: true,
+      port: 2468,
     },
-  },
+
+    plugins: [vue(), tailwindcss()],
+
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
+    },
+
+    css: {
+      preprocessorOptions: {
+        scss: {},
+      },
+    },
+  };
 });
